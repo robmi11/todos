7ac5f2e2fb272d6dafce4bfebca1c838855160a8
Robert Mizianty <robert.mizianty@gmail.com>
2023-02-08 19:22:48 +0100

    Base for CRUD operation done.

--- backend/controllers/todos.controllers.js
+++ backend/controllers/todos.controllers.js
@@ -1,5 +1,6 @@
 import TODOS from "../models/todos.models.js";
 import asyncHandler from "express-async-handler";
+import mongoose from "mongoose";
 
 /**
  * getAllTodos
@@ -29,4 +30,58 @@ export const setTodo = asyncHandler(async (req, res) => {
   }
 
   res.status(201).json(newTodo);
+});
+
+/**
+ * Update todo
+ */
+
+export const updateTodo = asyncHandler(async (req, res) => {
+  const { id } = req.params;
+  const { name } = req.body;
+  console.log(id);
+  if (!name) {
+    res.status(400);
+    throw new Error("Proszę podać nową wartość dla zadania.");
+  }
+
+  if (!id) {
+    res.status(400);
+    throw new Error("Nie przekazano id");
+  }
+
+  const todo = await TODOS.findById(id);
+  if (!todo) {
+    res.status(404);
+    throw new Error(`Zadanie o id ${id} nie zostało znalezione.`);
+  }
+
+  const updTodo = await TODOS.findByIdAndUpdate(id, { name }, { new: true });
+
+  if (!updTodo) {
+    res.status(500);
+    throw new Error("Błąd serwera");
+  }
+
+  res.status(200).json(updTodo);
+});
+
+/**
+ * deleteTodo
+ */
+export const deleteTodo = asyncHandler(async (req, res) => {
+  const { id } = req.params;
+
+  if (!id) {
+    res.status(400);
+    throw new Error("Proszę podać id.");
+  }
+
+  const deletedTodo = await TODOS.findByIdAndDelete(id);
+  if (!deletedTodo) {
+    res.status(500);
+    throw new Error("Błąd servera");
+  }
+
+  res.status(200).json({ message: `Zadanie numer ${id} zostało usunięte.` });
 });
--- backend/routes/todos.routes.js
+++ backend/routes/todos.routes.js
@@ -1,8 +1,14 @@
 import express from "express";
 const router = express.Router();
 
-import { getAllTodos, setTodo } from "../controllers/todos.controllers.js";
+import {
+  getAllTodos,
+  setTodo,
+  updateTodo,
+  deleteTodo,
+} from "../controllers/todos.controllers.js";
 
 router.route("/").get(getAllTodos).post(setTodo);
+router.route("/:id").delete(deleteTodo).put(updateTodo);
 
 export default router;
