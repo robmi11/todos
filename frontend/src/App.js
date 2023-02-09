60f67c3971400dd58a235062cf7ea9a58a31b5a4
Robert Mizianty <robert.mizianty@gmail.com>
2023-02-09 23:21:39 +0100

    Added forntend react and redux toolkit.

---
+++ frontend/src/App.js
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="container w-2/3 mx-auto shadow-2xl p-10">
+      <h1 className="text-3xl font-bold text-center">Witaj</h1>
+    </div>
+  );
+}
+
+export default App;
